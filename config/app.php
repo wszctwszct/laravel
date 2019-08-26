f7186200d8589f188f6a106f23b861bda02f6992
wszctwszct <jctnee@hotmail.com>
2019-08-26 20:43:21 +0800

    laravel runtime

--- config/app.php
+++ config/app.php
@@ -162,6 +162,8 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class, // Laravel IDE helper
+
         /*
          * Package Service Providers...
          */
--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,11 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+//Route::group(['prefix' => 'swagger'], function () {
+//    Route::get('json', 'SwaggerController@getJSON');
+//    Route::get('my-data', 'SwaggerController@getMyData');
+//});
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
